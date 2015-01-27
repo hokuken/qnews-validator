fc64a6a7b497c7d2d8ccb2f30884c7c6ab53a581
Shun Hotta <stars.big2men@gmail.com>
2015-01-27 12:38:38 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,131 @@
+# QNewsletter 登録フォーム検証スクリプト
+
+このスクリプトは Hokuken.Inc <customer@hokuken.com> が作成しました。
+MITライセンスを適用していますのでライセンスの範囲内で自由に複製、改変が可能です。
+
+
+## 概要
+
+QNewsletter の登録フォームに下記の検証機能を追加します。
+
+* メールアドレスと確認用メールアドレスの一致
+
+このスクリプトを読み込むと、上記の検証が通るまでフォームを送信することができなくなります。
+
+
+## ファイル
+
+下記のファイルが同梱されています。
+
+* README.md
+* validation.js
+* validation.min.js
+
+validation.min.js は validation.js を圧縮したものです。
+通常は .min.js の方を読み込みます。
+
+
+## インストール
+
+`http://example.jp` という架空のサイトへ設置する想定でインストール手順を解説します。
+
+
+1. スクリプトを設置
+
+validation.min.js を適当な場所へ置きます。
+
+例では設置先ドメインのルートフォルダに設置します。下記のようなURLでアクセスできます。
+`http://example.jp/validation.min.js`
+
+
+2. スクリプトを読み込む
+
+下記の1行をHTMLに貼り付けます。
+
+```
+<script src="/validation.min.js"></script>
+```
+
+`jquery.js` の読み込み箇所よりも後にしてください。
+
+
+3. インプット要素を追加する
+
+つづけてHTMLに確認用メールアドレスのインプット要素を追加します。
+
+```
+<input type="text" name="user/email2" value="" />
+```
+
+実際の作業としては、既に `user/email` の要素が存在するので、
+それを含む部分を丸ごとコピーして、 `name` 属性を `user/email2` へ書き換えます。
+
+
+4. エラーメッセージの表示先要素を追加する
+
+エラーメッセージを表示させたい部分へ下記のHTMLを追加します。
+
+```
+<div id="registFormErr" class="register-form-err"></div>
+```
+
+エラーが発生した場合、この中にエラーメッセージが挿入されます。
+スタイルを指定したい場合はCSSで `.register-form-err` に対してスタイルを指定してください。
+
+例：
+```
+<style>
+.register-form-err {
+	font-weight: bold;
+  border: 1px solid red;
+  color: red;
+  padding: 10px;
+}
+</style>
+```
+
+
+## カスタマイズ
+
+下記の部分をカスタマイズしていただけます。
+
+* 登録フォームのID （デフォルト： `#registForm` ）
+* エラーメッセージ （デフォルト：メールアドレスと確認用メールアドレスには同じ内容をご入力ください。）
+* 押せない「確認」ボタンのスタイル
+
+詳しい方法は下記で解説します。
+
+
+### 登録フォームのIDをカスタマイズする
+
+利用するスクリプトを `validate.min.js` から `validate.js` へ変更し、内容を編集してください。
+下記の部分（18行目）でフォームの要素を取得しているので任意の要素へ変更してください。
+
+```
+  var registerForm    = $("#registForm").get(0);
+```
+
+### エラーメッセージをカスタマイズする
+
+利用するスクリプトを `validate.min.js` から `validate.js` へ変更し、内容を編集してください。
+下記の部分（33行目）でフォームの要素を取得しているので任意の要素へ変更してください。
+
+```
+  var errEmailConfirmation = 'メールアドレスと確認用メールアドレスには同じ内容をご入力ください。';
+```
+
+### 押せない「確認」ボタンのスタイル
+
+検証が通るまで「確認」ボタンは押せなくなっていますが、見た目で分かりやすいようにしたい場合、ご自身でスタイルを定義できます。
+下記の例を参考にしてください。
+
+
+```
+<style>
+.qnews-locked-form input[type=submit],
+.qnews-locked-form button[type=submit] {
+	//任意のスタイル
+}
+</style>
+```
+
---
+++ validation.js
@@ -0,0 +1,143 @@
+/**
+ * Validation for QNewsletter
+ *
+ * 
+ */
+
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^\s+|\s+$/g,'');
+  };
+}
+
+$(function(){
+
+	// ----- 必要があれば編集してください -----
+
+			// フォーム
+  var registerForm    = $("#registForm").get(0),
+  
+  		// メールアドレスの入力欄
+  		email_input     = $('input:text[name="user/email"]').get(0),
+  		
+  		// 確認用メールアドレスの入力欄
+  		email2_input    = $('input:text[name="user/email2"]').get(0),
+  		
+  		// エラーメッセージの表示先
+  		err_placeholder = $('#registFormErr').get(0);
+
+			// フォームが送信不可の際に付けられるクラス名
+	var lockedClassName = "qnews-locked-form";
+
+			// validateEmailConfirmation で利用されるエラーメッセージ
+  var errEmailConfirmation = 'メールアドレスと確認用メールアドレスには同じ内容をご入力ください。';
+
+
+	// ----- 以下は編集しないでください -----
+
+	var validators = [];
+
+  // フォームを送信不可にする
+  var disableForm = function(){
+	$(registerForm)
+		.prop("disabled", true)
+		.addClass(lockedClassName)
+		.on("submit.qnews", function(e){
+			e.preventDefault();
+		});
+  };
+  
+  // フォームを送信可能にする
+  var enableForm = function(){
+	$(registerForm)
+		.prop("disabled", false)
+		.removeClass(lockedClassName)
+		.off("submit.qnews");
+	}
+
+	// フォームを検証可能にする
+	var enableValidation = function(){
+		disableForm();
+		$(registerForm).on("change", validateFormOnChange);
+	};
+
+	// フォームを検証し、結果により
+	// フォームを送信可能にしたり、
+	// エラーメッセージを表示したりする。
+	var validateFormOnChange = function(){
+		results = validate();
+		if (formIsOK(results)) {
+			clearErrs();
+			enableForm();
+		}
+		else showErrs(results);
+	};
+
+	// email と email2 の一致を検証する
+	// @return {success: boolean, err: string}
+	var validateEmailConfirmation = function(){
+		var email =  $(email_input).val().trim(),
+				email2 = $(email2_input).val().trim();
+		if (email.length === 0 || email2.length === 0) return;
+		if (email == email2) return {success: true, err: ''};
+		return {success: false, err: errEmailConfirmation};
+	};
+
+	// フォームを検証する
+	// @return array
+	var validate = function(callback){
+		return $.map(validators, function(validator){
+			return validator();
+		});
+	};
+
+	// フォームに入力された値が正しいかどうか
+	// @param array from validate
+	// @return boolean
+	var formIsOK = function(results){
+		if (results.length === 0) return false;
+		var base = undefined;
+		$.each(results, function(i, result){
+			if (typeof result === "undefined") return;
+			if (typeof base === "undefined") {
+				base = result.success;
+			} else {
+				base = base && result.success;
+			}
+		});
+		if (typeof base === "undefined") return false;
+		return base;
+	};
+
+	// エラーメッセージのみ抽出する
+	// @param array from validate
+	// @return array of errs
+	var getErrs = function(results){
+		return $.map(results, function(result){
+			if ( ! result.success) {
+				return result.err;
+			}
+		});
+	};
+
+	// 要素 err_placeholder 内にエラーメッセージを表示する
+	// @param array from validate
+	var showErrs = function(results){
+		var errs = getErrs(results);
+		$(err_placeholder).html(errs.join("<br>\n"));
+	};
+
+	// 要素 err_placeholder 内のエラーメッセージを消去する
+	var clearErrs = function(){
+		$(err_placeholder).empty();
+	};
+
+	// メインロジック
+	var main = function(){
+		validators.push(validateEmailConfirmation);
+		enableValidation();
+	};
+
+	main();
+
+});
